5d0a047118bfcac3d735187a72589a980c1d8700
Palmer Truelson <truelson@gmail.com>
2013-07-27 08:20:36 +0200

    works decently

--- anki_import_file_builder.rb
+++ anki_import_file_builder.rb
@@ -7,7 +7,10 @@ require 'easy_translate'
 require 'google-search'
 require 'open-uri'
 
-input_file_name, output_file_name = ARGV
+string_lang, word_type, INPUT_FILE_NAME, OUTPUT_FILE_NAME, skip_images = ARGV
+LANGUAGE = string_lang.to_sym
+WORD_TYPE = word_type.downcase
+SKIP_IMAGES = ( skip_images == "--skip-images" )
 
 CONFIG = YAML::load_file( File.join( __dir__, 'config.yml' ))
 
@@ -21,35 +24,58 @@ def addThe( word_array )
   the_array = word_array.map { |word| "the #{word}" }
 end
 
-def getSingleWord( word_set_array )
-  word_set_array.map { |word_set| word_set.scan( /[[:alpha:]]+/ )[1].downcase }
-end
-
-def downloadImage( word )
-  images = Google::Search::Image.new( :query => word, :as_filetype => :jpg, :size => :small, :api_key => CONFIG["google_api_key"] )
-  image_url = images.first.uri
-  File.open( "images/#{word}.jpg", "w" ) do |file|
-    file.print open( image_url ).read
+def getSingleWordArray( word_set_array )
+  if ( WORD_TYPE == 'noun' ) 
+    word_set_array.map { |word_set| word_set.scan( /[[:alpha:]]+/ )[1].downcase }
+  else
+    word_set_array
   end
 end
 
-File.open input_file_name, 'r' do |input_file|
-  words = input_file.readline.chomp
-  word_array = words.split(/,\s*/)
+def expandForNoun( word_array )
+
   plural_array = toPluralArray( word_array )
 
   the_word_array = addThe( word_array )
   the_plural_array = addThe( plural_array )
 
-  english_array = the_word_array.zip( the_plural_array ).map { |pair| pair.join(', ') }
-  foreign_array = EasyTranslate.translate( english_array, :to => :german, :key => CONFIG["google_api_key"] )
+  the_word_array.zip( the_plural_array ).map { |pair| pair.join(' / ') }
 
-  single_foreign_word_array = getSingleWord( foreign_array )
+end
 
-  File.open output_file_name, 'w' do |output_file|
-    foreign_array.zip( single_foreign_word_array, word_array ).map do |pair|
-      output_file.write( "#{pair[0]}; <img src=#{pair[2]}.jpg />\n" )
-      downloadImage( pair[1] )
+def downloadImage( source_word, foreign_word )
+  images = Google::Search::Image.new( :query => foreign_word,
+                                      :language => LANGUAGE,
+                                      :api_key => CONFIG["google_api_key"],
+                                      :as_filetype => :jpg )
+
+  image_url = images.first.uri
+  File.open( "images/#{source_word}.jpg", "w" ) do |file|
+    file.print open( image_url ).read
+  end
+end
+
+File.open INPUT_FILE_NAME, 'r' do |input_file|
+  words = input_file.readline.chomp
+  word_array = words.split(/,\s*/)
+
+  if ( WORD_TYPE == "noun" )
+    source_array = expandForNoun word_array
+  elsif ( WORD_TYPE == 'verb' )
+    source_array = word_array
+  else
+    source_array = word_array
+  end
+
+  foreign_array = EasyTranslate.translate( source_array, :to => LANGUAGE, :key => CONFIG["google_api_key"] )
+
+  single_foreign_word_array = getSingleWordArray( foreign_array )
+
+  File.open OUTPUT_FILE_NAME, 'w' do |output_file|
+    foreign_array.zip( word_array, single_foreign_word_array ).map do |triple|
+      output_file.write( "<img src=\"#{triple[1]}.jpg\" />; #{triple[0]}\n" )
+      puts triple[1], triple[2]
+      downloadImage( triple[1], triple[2] ) unless SKIP_IMAGES
     end
   end
 end
--- build_anki_cards.sh
+++ build_anki_cards.sh
@@ -1,5 +1,12 @@
 #1/bin/sh
 
-cp -f ./images/* ~/Documents/Anki/User\ 1/collection.media/
+# Exit out if they don't have 3 arguments
+if [ $# -ne 4 ] && [ $# -ne 5 ]; then
+  echo "Usage: $0 <translate-to-language-code(like 'de')> <word-type(like 'noun')> <word-list-file> <anki-import-file> [--skip-images]"
+  exit 1
+fi
 
-bundle exec ./anki_import_file_builder.rb $1 $2
+bundle exec ./anki_import_file_builder.rb $1 $2 $3 $4 $5
+
+open ./images
+#cp -f ./images/* ~/Documents/Anki/User\ 1/collection.media/
